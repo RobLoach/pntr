91525349cd7146b9047755643420a973efe8beb3
Rob Loach <robloach@gmail.com>
2023-08-10 23:02:33 -0400

    Clean up sqrtf

--- pntr.h
+++ pntr.h
@@ -831,13 +831,12 @@ extern "C" {
         float _pntr_sqrtf(float number) {
             long i;
             float x2, y;
-            const float threehalfs = 1.5f;
             x2 = number * 0.5f;
             y  = number;
             i  = *(long*)&y;
             i  = 0x5f3759df - (i >> 1);
             y  = *(float*)&i;
-            y  = y * (threehalfs - (x2 * y * y));
+            y  = y * (1.5f - (x2 * y * y)); // threehalfs
             return 1.0f / y;
         }
         */
