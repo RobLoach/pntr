2b6c7087c5e9ccc2d2e608c5fd267ea3c008078c
Rob Loach <robloach@gmail.com>
2023-09-24 06:12:15 -0400

    Switch to stb_image (#129)

--- pntr.h
+++ pntr.h
@@ -751,6 +751,10 @@ extern "C" {
     #define PNTR_MEMCPY(dest, src, n) memcpy((void*)(dest), (const void*)(src), (size_t)(n))
 #endif  // PNTR_MEMCPY
 
+#ifndef PNTR_MEMMOVE
+    #define PNTR_MEMMOVE(dest, src, n) PNTR_MEMCPY(dest, src, n)
+#endif  // PNTR_MEMCPY
+
 #ifndef PNTR_MEMSET
     #include <string.h>
     /**
@@ -938,58 +942,6 @@ extern "C" {
         #define PNTR_PIXELFORMAT PNTR_PIXELFORMAT_ARGB8888
     #endif
 #endif  // PNTR_PIXELFORMAT
-
-// cute_png
-#if !defined(PNTR_SAVE_IMAGE_TO_MEMORY) || !defined(PNTR_LOAD_IMAGE_FROM_MEMORY)
-    #ifndef PNTR_NO_CUTE_PNG_IMPLEMENTATION
-        #define CUTE_PNG_IMPLEMENTATION
-        #define CUTE_PNG_ALLOCA PNTR_MALLOC
-        #define CUTE_PNG_ALLOC PNTR_MALLOC
-        #define CUTE_PNG_FREE PNTR_FREE
-        #define CUTE_PNG_CALLOC(num, size) PNTR_MALLOC((num) * (size))
-        #define CUTE_PNG_REALLOC PNTR_REALLOC
-        #define CUTE_PNG_MEMCPY PNTR_MEMCPY
-        #define CUTE_PNG_MEMSET PNTR_MEMSET
-        #define CUTE_PNG_FPRINTF (void)
-        #define CUTE_PNG_ASSERT(condition) 0 // Skip assertions
-        #define CUTE_PNG_SEEK_SET 0
-        #define CUTE_PNG_SEEK_END 0
-        #define CUTE_PNG_FILE void
-        #define CUTE_PNG_FOPEN(filename, mode) (CUTE_PNG_FILE*)filename
-        #define CUTE_PNG_FSEEK(stream, offset, origin) offset
-        #define CUTE_PNG_FREAD(data, size, num, fp) (void)(data)
-        #define CUTE_PNG_FTELL(fp) 0
-        #define CUTE_PNG_FWRITE(data, size, num, fp) (void)(data)
-        #define CUTE_PNG_FCLOSE (void)
-        #define CUTE_PNG_FERROR(fp) 1
-        #define CUTE_PNG_ATLAS_MUST_FIT 1
-        #define CUTE_PNG_ATLAS_FLIP_Y_AXIS_FOR_UV 0
-        #define CUTE_PNG_ATLAS_EMPTY_COLOR 0
-    #endif  // PNTR_NO_CUTE_PNG_IMPLEMENTATION
-
-    #if defined(__GNUC__) || defined(__clang__)
-        #pragma GCC diagnostic push
-        #pragma GCC diagnostic ignored "-Wpragmas"
-        #pragma GCC diagnostic ignored "-Wunknown-pragmas"
-        #pragma GCC diagnostic ignored "-Wsign-conversion"
-        #pragma GCC diagnostic ignored "-Wconversion"
-        #pragma GCC diagnostic ignored "-Wunused-function"
-        #pragma GCC diagnostic ignored "-Wunused-variable"
-        #pragma GCC diagnostic ignored "-Wsign-compare"
-        #pragma GCC diagnostic ignored "-Wunused-value"
-    #endif // defined(__GNUC__) || defined(__clang__)
-
-    #include "external/cute_png.h"
-    #define PNTR_NO_CUTE_PNG_IMPLEMENTATION
-    #ifdef CUTE_PNG_IMPLEMENTATION
-        #undef CUTE_PNG_IMPLEMENTATION
-    #endif
-
-    #if defined(__GNUC__) || defined(__clang__)
-        #pragma GCC diagnostic pop
-    #endif // defined(__GNUC__) || defined(__clang__)
-
-#endif // !defined(PNTR_SAVE_IMAGE_TO_MEMORY) || !defined(PNTR_LOAD_IMAGE_FROM_MEMORY)
 
 // STB TrueType
 #ifdef PNTR_ENABLE_TTF
@@ -2261,6 +2213,16 @@ PNTR_API pntr_color pntr_image_get_color(pntr_image* image, int x, int y) {
     return PNTR_PIXEL(image, x, y);
 }
 
+// Load stb_image or cute_png.
+#ifndef PNTR_LOAD_IMAGE_FROM_MEMORY
+    #include "extensions/pntr_stb_image.h"
+#endif
+
+#ifndef PNTR_SAVE_IMAGE_TO_MEMORY
+    //#include "extensions/pntr_cute_png.h"
+    #include "extensions/pntr_stb_image_write.h"
+#endif
+
 /**
  * Load an image from memory buffer.
  *
@@ -2273,19 +2235,7 @@ PNTR_API pntr_image* pntr_load_image_from_memory(const unsigned char *fileData, 
         return pntr_set_error(PNTR_ERROR_INVALID_ARGS);
     }
 
-    #if defined(PNTR_LOAD_IMAGE_FROM_MEMORY)
-        return PNTR_LOAD_IMAGE_FROM_MEMORY(fileData, dataSize);
-    #else
-        cp_image_t image = cp_load_png_mem(fileData, (int)dataSize);
-        if (image.pix == NULL) {
-            return pntr_set_error(PNTR_ERROR_FAILED_TO_OPEN);
-        }
-
-        pntr_image* output = pntr_image_from_pixelformat((const void*)image.pix, image.w, image.h, PNTR_PIXELFORMAT_RGBA8888);
-        cp_free_png(&image);
-
-        return output;
-    #endif
+    return PNTR_LOAD_IMAGE_FROM_MEMORY(fileData, dataSize);
 }
 
 /**
@@ -3802,7 +3752,7 @@ PNTR_API void* pntr_image_to_pixelformat(pntr_image* image, unsigned int* dataSi
  * @param image The image to save to memory.
  * @param dataSize Where to put the resulting size of the image. Use NULL if you do not care about getting the file size.
  *
- * @return The image data in memory. This data must be freed when finished using.
+ * @return The image data in memory. This data must be freed when finished using. NULL on failure.
  *
  * @see PNTR_SAVE_IMAGE_TO_MEMORY
  */
@@ -3811,35 +3761,7 @@ PNTR_API unsigned char* pntr_save_image_to_memory(pntr_image* image, unsigned in
         return pntr_set_error(PNTR_ERROR_INVALID_ARGS);
     }
 
-    #ifdef PNTR_SAVE_IMAGE_TO_MEMORY
-        return PNTR_SAVE_IMAGE_TO_MEMORY(image, dataSize);
-    #else
-        cp_image_t cpImage = PNTR_CLITERAL(cp_image_t) {
-            .w = image->width,
-            .h = image->height
-        };
-
-        cpImage.pix = (cp_pixel_t*)pntr_image_to_pixelformat(image, NULL, PNTR_PIXELFORMAT_RGBA8888);
-        if (cpImage.pix == NULL) {
-            return NULL;
-        }
-
-        cp_saved_png_t png = cp_save_png_to_memory(&cpImage);
-        if (png.data == NULL) {
-            cp_free_png(&cpImage);
-            return pntr_set_error(PNTR_ERROR_FAILED_TO_WRITE);
-        }
-
-        // Export the datasize
-        if (dataSize != NULL) {
-            *dataSize = (unsigned int)png.size;
-        }
-
-        // Free up the temporary copy
-        cp_free_png(&cpImage);
-
-        return (unsigned char*)png.data;
-    #endif  // PNTR_SAVE_IMAGE_TO_MEMORY
+    return PNTR_SAVE_IMAGE_TO_MEMORY(image, dataSize);
 }
 
 /**
